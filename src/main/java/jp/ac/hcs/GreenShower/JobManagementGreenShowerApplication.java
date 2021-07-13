323ead14e3a36d1832c498269f044c3f494614b5
rewri <rewri@DESKTOP-C6NDBBL>
2021-07-13 09:56:11 +0900

    最初のコミット

---
+++ src/main/java/jp/ac/hcs/GreenShower/JobManagementGreenShowerApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.GreenShower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagementGreenShowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagementGreenShowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/GreenShower/JobManagementGreenShowerApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.GreenShower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobManagementGreenShowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
