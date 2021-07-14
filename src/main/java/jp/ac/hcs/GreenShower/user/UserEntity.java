1231d065c68c7591e080e53617f9b09b7e5f8981
rewri <rewri@DESKTOP-C6NDBBL>
2021-07-14 10:28:44 +0900

    UserEntity

--- src/main/java/jp/ac/hcs/GreenShower/user/UserEntity.java
+++ src/main/java/jp/ac/hcs/GreenShower/user/UserEntity.java
@@ -1,5 +1,15 @@
 package jp.ac.hcs.GreenShower.user;
 
-public class UserEntity {
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.Data;
+
+/**
+ * ユーザ情報をまとめて管理する為のエンティティクラス
+ */
+@Data
+public class UserEntity {
+	/** ユーザ情報のリスト */
+	private List<UserData> userlist = new ArrayList<UserData>();
 }
