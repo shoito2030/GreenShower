c5b28fae0a2b875e3a4b59d12a871f363e6a6083
rewri <rewri@DESKTOP-C6NDBBL>
2021-07-14 11:40:18 +0900

    crassroom修正

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,5 +1,5 @@
 /* ユーザマスタのデータ（一般権限：学生） PASS:password */
-INSERT INTO users (user_id, encrypted_password, name, role, class, class_number, register_user_id)
+INSERT INTO users (user_id, encrypted_password, name, role, classroom, class_number, register_user_id)
 VALUES('isida@xxx.co.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', '石田悠介', 'STUDENT', 'S3A1', '01', 'isida@xxx.co.jp');
 
 /* ユーザマスタのデータ（ADMIN権限：担任、事務） PASS:password */
