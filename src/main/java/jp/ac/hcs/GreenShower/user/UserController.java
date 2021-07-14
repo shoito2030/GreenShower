4f9152a55fea4ba760ccef19e071d7cbd2169ea7
chocobaby727 <chocobabion1152@gmail.com>
2021-07-14 10:46:49 +0900

    ユーザ：コントローラー、サービス完成（仮）

--- src/main/java/jp/ac/hcs/GreenShower/user/UserController.java
+++ src/main/java/jp/ac/hcs/GreenShower/user/UserController.java
@@ -1,5 +1,33 @@
 package jp.ac.hcs.GreenShower.user;
 
-public class UserController {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping("/user/list")
+	public String getUserList(Model model) {
+
+		// ユーザIDに紐づく情報を取得（取得できなかった場合は空のOptionalが格納される）
+		Optional<UserEntity> userEntity = userService.selectAll();
+
+		// 処理失敗によりトップ画面へ
+		if (userEntity.isEmpty()) {
+			return "index";
+		}
+
+		model.addAttribute("userEntity", userEntity.get());
+		return "user/userList";
+	}
 }
