dbf987058c3ecd50e9b8df4808e753fb22922f85
rewri <rewri@DESKTOP-C6NDBBL>
2021-07-14 12:25:47 +0900

    Report

--- src/main/java/jp/ac/hcs/GreenShower/report/ReportData.java
+++ src/main/java/jp/ac/hcs/GreenShower/report/ReportData.java
@@ -1,5 +1,34 @@
 package jp.ac.hcs.GreenShower.report;
 
-public class ReportData {
+import lombok.Data;
 
+@Data
+public class ReportData {
+	/**
+	 * 受験報告ID
+	 */
+	private String report_id;
+	
+	/**
+	 * ユーザID(メールアドレス)
+	 * メールアドレス形式
+	 */
+	private String user_id;
+	
+	/**
+	 * 所属クラス
+	 * 固定長文字列(4)
+	 */
+	private String classroom;
+	
+	/**
+	 * 出席番号
+	 * 固定長文字列(2)
+	 */
+	private String class_number;
+	
+	/**
+	 * ユーザ名（氏名）
+	 */
+	private String name;
 }
