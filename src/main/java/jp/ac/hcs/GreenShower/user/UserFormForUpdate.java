9257eb4e94f9fdb349a823a736d39a7f8734fdcf
chocobaby727 <chocobabion1152@gmail.com>
2021-07-15 11:39:39 +0900

    受験報告新規登録画面遷移成功

--- src/main/java/jp/ac/hcs/GreenShower/user/UserFormForUpdate.java
+++ src/main/java/jp/ac/hcs/GreenShower/user/UserFormForUpdate.java
@@ -1,12 +1,6 @@
 package jp.ac.hcs.GreenShower.user;
 
-import java.util.Date;
-
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-
-import org.hibernate.validator.constraints.Length;
 
 import lombok.Data;
 
@@ -16,22 +10,10 @@ import lombok.Data;
 @Data
 public class UserFormForUpdate {
 
-	/** ユーザID（メールアドレス） */
-	@NotBlank(message = "{require_check}")
-	@Email(message = "{email_check}")
-	private String user_id;
-
-	/** パスワード */
-	@Length(min = 4, max = 100)
-	@Pattern(regexp = "/^[0-9a-zA-Z]*$/	")
-	private String encrypted_password;
-
 	/** ユーザ名 */
 	@NotBlank(message = "{require_check}")
 	private String name;
-
-	private boolean dark_mode;
-
+	
 	/**
 	 * 権限 - 学生 : "ROLE_STUDENT" - 担任 : "ROLE_TEACHER" - 事務 : "ROLE_STAFF" 必須入力
 	 */
@@ -46,26 +28,6 @@ public class UserFormForUpdate {
 	 * 出席番号 固定長文字列(2)
 	 */
 	private String class_number;
-
-	/**
-	 * 登録日時 登録時に取得したタイムスタンプ（日時をミリ秒まで）
-	 */
-	private Date register_date;
-
-	/**
-	 * 登録者のユーザID ユーザマスタのユーザIDを参照
-	 */
-	private String register_user_id;
-
-	/**
-	 * 更新者のユーザID 更新時に取得したタイムスタンプ（日時をミリ秒まで） 更新が行われるまでNULL
-	 */
-	private Date update_date;
-
-	/**
-	 * 更新者のユーザID ユーザマスタのユーザIDを参照 更新が行われるまでNULL
-	 */
-	private String update_user_id;
 
 	/**
 	 * アカウント有効性 - 有効 : true - 無効 : false ユーザ作成時はtrue固定
