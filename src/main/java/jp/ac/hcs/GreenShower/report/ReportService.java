9145679ed089659ab3fd4d5495e620dfeeae46b4
chocobaby727 <chocobabion1152@gmail.com>
2021-07-16 12:14:50 +0900

    途中経過保存

--- src/main/java/jp/ac/hcs/GreenShower/report/ReportService.java
+++ src/main/java/jp/ac/hcs/GreenShower/report/ReportService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import jp.ac.hcs.GreenShower.user.UserFormForInsert;
+
 
 @Service
 public class ReportService {
@@ -48,6 +50,42 @@ public class ReportService {
 		}
 
 		return Optional.ofNullable(reportEntity);
+	}
+	
+	/**
+	 * ユーザマスタに新たなユーザ情報を1件追加する
+	 * 
+	 * @param form             検証済み入力情報
+	 * @param register_user_id 登録処理を実行したユーザのID
+	 * @return - true：追加件数1件以上（処理成功）の場合 - false：追加件数0件（処理失敗）の場合
+	 */
+	public boolean insert(UserFormForInsert form, String register_user_id) {
+		int rowNumber = 0;
+
+		try {
+			// 追加処理を行い、追加できた件数を取得
+			rowNumber = reportRepository.insertOne(refillToReportData(form, register_user_id));
+		} catch (DataAccessException e) {
+			e.printStackTrace();
+		}
+		return rowNumber > 0;
+	}
+	
+	
+	/**
+	 * 入力情報をUserData型に変換する（insert用）
+	 * 
+	 * @param form    検証済み入力データ
+	 * @param user_id 登録処理を実行したユーザのID
+	 * @return UserData
+	 */
+	private ReportData refillToReportData(UserFormForInsert form, String register_user_id) {
+		ReportData data = new ReportData();
+
+		
+		data.setRegistered_user_id(register_user_id);
+
+		return data;
 	}
 
 }
