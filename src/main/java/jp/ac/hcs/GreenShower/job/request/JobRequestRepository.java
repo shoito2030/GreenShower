7d42b9cc5619ace281143383369a8d0426b5388a
s20193050 <s20193050@HCS19015.stdlan.hcs.local>
2021-10-25 14:27:58 +0900

    CSV出力処理途中経過

--- src/main/java/jp/ac/hcs/GreenShower/job/request/JobRequestRepository.java
+++ src/main/java/jp/ac/hcs/GreenShower/job/request/JobRequestRepository.java
@@ -9,13 +9,11 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import jp.ac.hcs.GreenShower.report.ReportRowCallbackHandler;
-
 @Repository
 public class JobRequestRepository {
 	
 	/** SQL 全件取得（クラスと出席番号の昇順） */
-	private static final String SQL_SELECT_ALL = "SELECT * FROM jobrequest";
+	private static final String SQL_SELECT_ALL = "SELECT * FROM requests";
 	
 	@Autowired
 	private JdbcTemplate jdbc;
@@ -27,10 +25,10 @@ public class JobRequestRepository {
 	 * @param user_id 検索するユーザID
 	 * @throws DataAccessException
 	 */
-	public void reportlistCsvOut() throws DataAccessException {
+	public void requestlistCsvOut() throws DataAccessException {
 
 		// CSVファイル出力用設定
-		ReportRowCallbackHandler handler = new ReportRowCallbackHandler();
+		JobRequestRowCallbackHandler handler = new JobRequestRowCallbackHandler();
 
 		jdbc.query(SQL_SELECT_ALL, handler);
 	}
@@ -41,23 +39,38 @@ public class JobRequestRepository {
 		JobRequestEntity jobRequestEntity = mappingSelectResult(resultList);
 		return jobRequestEntity;
 	}
-
-
+	
+	/**
+	 * requestsテーブルから取得したデータをJobRequsettEntity形式にマッピングする.
+	 * 
+	 * @param resultList reportテーブルから取得したデータ
+	 * @return JobRequestEntity
+	 */
 	private JobRequestEntity mappingSelectResult(List<Map<String, Object>> resultList) {
 		JobRequestEntity entity = new JobRequestEntity();
 		
 		for (Map<String, Object> map : resultList) {
 			JobRequestData data = new JobRequestData();
 			
-			data.setRequest_type((String) map.get("request_type"));
+			data.setApply_id((String) map.get("apply_id"));
+			data.setApplicant_id((String) map.get("applicant_id"));
+			data.setStatus((String) map.get("status"));
+			data.setApply_type((String) map.get("apply_type"));
+			data.setIndicate((String) map.get("indicate"));
 			data.setCompany_name((String) map.get("company_name"));
-			data.setStart((Date) map.get("start"));
-			data.setEnd((Date) map.get("end"));
-			data.setZipcode((String) map.get("zipcode"));
-			data.setPlace((String) map.get("place"));
+			data.setDate_activity_from((Date) map.get("date_activity_from"));
+			data.setDate_activity_to((Date) map.get("date_activity_to"));
+			data.setLoc((String) map.get("loc"));
 			data.setMeans((String) map.get("means"));
-			data.setMemorandum((String) map.get("memorandum"));
-			
+			data.setRemark((String) map.get("remark"));
+			data.setDate_absence_from((Date) map.get("date_absence_from"));
+			data.setDate_absence_to((Date) map.get("date_absence_to"));
+			data.setLeave_early_date((Date) map.get("leave_early_date"));
+			data.setAttendance_date((Date) map.get("attendance_date"));
+			data.setRegister_date((Date) map.get("register_date"));
+			data.setRegister_user_id((String) map.get("register_user_id"));
+			data.setUpdate_date((Date) map.get("update_date"));
+			data.setUpdate_user_id((String) map.get("update_user_id"));
 		}
 		return null;
 	}
