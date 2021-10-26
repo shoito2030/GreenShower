ba440a9fd609a41445e971b949be671f1b9e4e51
s20192030 <s20192030@HCS19007.stdlan.hcs.local>
2021-10-26 09:29:51 +0900

    re

--- src/main/java/jp/ac/hcs/GreenShower/job/request/JobRequestRepository.java
+++ src/main/java/jp/ac/hcs/GreenShower/job/request/JobRequestRepository.java
@@ -15,7 +15,7 @@ import jp.ac.hcs.GreenShower.report.ReportRowCallbackHandler;
 public class JobRequestRepository {
 	
 	/** SQL 全件取得（クラスと出席番号の昇順） */
-	private static final String SQL_SELECT_ALL = "SELECT * FROM jobrequest";
+	private static final String SQL_SELECT_ALL = "SELECT * FROM JOB_HUNTING JOIN REQUESTS USING(APPLY_ID);";
 	
 	@Autowired
 	private JdbcTemplate jdbc;
@@ -49,16 +49,26 @@ public class JobRequestRepository {
 		for (Map<String, Object> map : resultList) {
 			JobRequestData data = new JobRequestData();
 			
-			data.setRequest_type((String) map.get("request_type"));
+			data.setApply_id((String) map.get("apply_id"));
+			data.setApplicant_id((String) map.get("applicant_id"));
+			data.setStatus((String) map.get("status"));
+			data.setApply_type((String) map.get("apply_type"));
+			data.setIndicate((String) map.get("indicate"));
 			data.setCompany_name((String) map.get("company_name"));
-			data.setStart((Date) map.get("start"));
-			data.setEnd((Date) map.get("end"));
-			data.setZipcode((String) map.get("zipcode"));
-			data.setPlace((String) map.get("place"));
+			data.setDate_activity_from((Date) map.get("date_activity_from"));
+			data.setDate_activity_to((Date) map.get("date_activity_to"));
+			data.setLoc((String) map.get("loc"));
 			data.setMeans((String) map.get("means"));
-			data.setMemorandum((String) map.get("memorandum"));
-			
-		}
+			data.setRemark((String) map.get("remark"));
+			data.setDate_absence_from((Date) map.get("date_absence_from"));
+			data.setDate_absence_to((Date) map.get("date_absence_to"));
+			data.setLeave_early_date((Date) map.get("leave_early_date"));
+			data.setLeave_early_date((Date) map.get("leave_early_date"));
+			data.setAttendance_date((Date) map.get("attendance_date"));
+			data.setRegister_date((Date) map.get("register_date"));
+			data.setRegister_user_id((String) map.get("register_user_id"));		
+			data.setUpdate_date((Date) map.get("update_date"));
+			data.setUpdate_user_id((String) map.get("update_user_id"));		}
 		return null;
 	}
 }
