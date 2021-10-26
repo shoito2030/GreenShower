3aee54945eda4d4f1d77832d534f5c0ad18195a3
s20193050 <s20193050@HCS19015.stdlan.hcs.local>
2021-10-26 10:13:27 +0900

    報告一覧画面遷移

--- src/main/java/jp/ac/hcs/GreenShower/job/csv/JobCsvRequestRepository.java
+++ src/main/java/jp/ac/hcs/GreenShower/job/csv/JobCsvRequestRepository.java
@@ -11,8 +11,8 @@ public class JobCsvRequestRepository {
 	/** SQL 申請情報全件取得 */
 	private static final String SQL_SELECT_ALL = "SELECT * FROM REQUESTS;";
 	
-	/** SQL 申請情報全件取得 */
-	private static final String SQL_SELECT_ALL_REQUESTS = "SELECT * FROM JOB_HUNTING JH, REQUESTS REQ, USERS U WHERE JH.APPLY_ID = REQ.APPLY_ID AND JH.APPLICANT_ID  = U.USER_ID;";
+	/** CSV出力用SQL */
+	private static final String SQL_SELECT_CSV = "SELECT * FROM JOB_HUNTING JH,REQUESTS REQ, REPORTS REP, USERS U WHERE JH.APPLY_ID = REQ.APPLY_ID AND JH.APPLY_ID = REP.APPLY_ID AND JH.APPLICANT_ID = U.USER_ID;";
 	
 	@Autowired
 	private JdbcTemplate jdbc;
@@ -28,6 +28,6 @@ public class JobCsvRequestRepository {
 		// CSVファイル出力用設定
 		JobCsvRequestRowCallbackHandler handler = new JobCsvRequestRowCallbackHandler();
 
-		jdbc.query(SQL_SELECT_ALL_REQUESTS, handler);
+		jdbc.query(SQL_SELECT_CSV, handler);
 	}
 }
