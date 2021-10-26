10a786ed706c6794acc9d1e92d6fb8467e52c390
s20192030 <s20192030@HCS19007.stdlan.hcs.local>
2021-10-26 12:29:14 +0900

    報告機能作成途中

--- src/main/java/jp/ac/hcs/GreenShower/job/report/JobReportRepository.java
+++ src/main/java/jp/ac/hcs/GreenShower/job/report/JobReportRepository.java
@@ -1,18 +1,78 @@
 package jp.ac.hcs.GreenShower.job.report;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import jp.ac.hcs.GreenShower.job.common.CommonEnum;
+import jp.ac.hcs.GreenShower.job.common.JobHuntingData;
 
 @Repository
 public class JobReportRepository {
-
+	
+	/** SQL 報告情報全件取得 */
+	private static final String SQL_SELECT_ALL_REPORTS = "SELECT * FROM JOB_HUNTING JH, REQUESTS REQ, REPORTS REP, USERS U WHERE JH.APPLY_ID = REQ.APPLY_ID AND JH.APPLY_ID = REP.APPLY_ID AND JH.APPLICANT_ID  = U.USER_ID ORDER BY JH.STATUS;";
+	
+	/** 特定のユーザ一人分の報告情報取得 */
+	private static final String SQL_SELECT_STUDENT_REPORTS = "SELECT * FROM JOB_HUNTING JH, REQUESTS REQ, REPORTS REP, USERS U WHERE JH.APPLY_ID = REQ.APPLY_ID AND JH.APPLY_ID = REP.APPLY_ID AND JH.APPLICANT_ID  = U.USER_ID AND JH.APPLICANT_ID = ? ORDER BY JH.STATUS;";
+	@Autowired
+	private JdbcTemplate jdbc;
+	
+	
 	public JobReportEntity selectAllReports() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		List<Map<String, Object>> resultList = jdbc.queryForList(SQL_SELECT_ALL_REPORTS);
+		JobReportEntity jobReportEntity = mappingSelectResult(resultList);
+
+		return jobReportEntity;
 	}
 
 	public JobReportEntity selectStudentReports(String name) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		List<Map<String, Object>> resultList = jdbc.queryForList(SQL_SELECT_STUDENT_REPORTS, name);
+		JobReportEntity jobReportEntity = mappingSelectResult(resultList);
+
+		return jobReportEntity;
+	}
+	
+	/**
+	 * job_huntingテーブルとrequestsテーブルから取得したデータをJobRequsettEntity形式にマッピングする.
+	 * 
+	 * @param resultList report job_huntingテーブルとrequestsテーブルから取得したデータ
+	 * @return JobRequestEntity
+	 */
+	private JobReportEntity mappingSelectResult(List<Map<String, Object>> resultList) {
+		JobReportEntity entity = new JobReportEntity();
+		
+		for (Map<String, Object> map : resultList) {
+			JobReportData data = new JobReportData();
+
+			// JobHuntingDataクラスのフィールドを補完（protectedなフィールド）
+			data.setClassroom((String) map.get("classroom"));
+			data.setClass_number((String) map.get("class_number"));
+			data.setName((String) map.get("name"));
+			data.setApply_id((String) map.get("apply_id"));
+			data.setApplicant_id((String) map.get("applicant_id"));
+			data.setStatus(CommonEnum.getEnum(JobHuntingData.Status.class,(String) map.get("status")));
+			data.setApply_type(CommonEnum.getEnum(JobHuntingData.Apply_type.class, (String) map.get("apply_type")));
+			data.setIndicate((String) map.get("indicate"));
+			
+			
+			// JobRequestDataクラスのフィールドを補完
+			data.setRemark((String) map.get("remark"));
+			
+			
+			// システム内で使用しないので取得する必要ないかも（応相談）
+//			data.setRegister_date((Date) map.get("register_date"));
+//			data.setRegister_user_id((String) map.get("register_user_id"));
+//			data.setUpdate_date((Date) map.get("update_date"));
+//			data.setUpdate_user_id((String) map.get("update_user_id"));
+			entity.getJobReportList().add(data);
+		}
+		
+		return entity;
+
 	}
 
 }
