00070a367809e2ebd64cd23cf89cb3c69a673177
s20192030 <s20192030@HCS19007.stdlan.hcs.local>
2021-11-08 10:41:06 +0900

    Eventform

--- src/main/java/jp/ac/hcs/GreenShower/job/request/EventForm.java
+++ src/main/java/jp/ac/hcs/GreenShower/job/request/EventForm.java
@@ -1,5 +1,39 @@
 package jp.ac.hcs.GreenShower.job.request;
 
-public class EventForm {
+import javax.validation.constraints.NotBlank;
 
+import jp.ac.hcs.GreenShower.job.validations.DateValueValid;
+import lombok.Data;
+
+@Data
+public class EventForm {
+	/**
+	 * 企業名
+	 */
+	@NotBlank
+	private String company_name;
+	
+	/**
+	 * 開始時刻
+	 */
+	@NotBlank
+	@DateValueValid
+	private String datetime;
+	
+	/**
+	 * 場所
+	 */
+	@NotBlank
+	private String loc;
+	
+	/**
+	 * 内容
+	 */
+	@NotBlank
+	private String content;
+	
+	/**
+	 * 内容
+	 */
+	private String bring;
 }
