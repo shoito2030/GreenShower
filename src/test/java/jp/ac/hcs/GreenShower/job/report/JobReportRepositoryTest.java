7398fea8e70f1fafc493b32a1ba1f7514090448c
s20192030 <s20192030@HCS19007.stdlan.hcs.local>
2021-11-15 11:51:46 +0900

    テストファイル作成

---
+++ src/test/java/jp/ac/hcs/GreenShower/job/report/JobReportRepositoryTest.java
@@ -0,0 +1,124 @@
+package jp.ac.hcs.GreenShower.job.report;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JobReportRepositoryTest {
+
+	@Test
+	void testSelectAllReports() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testSelectOne() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testSelectPersonalInfoApply() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testSelectPersonalInfoUserID() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testSelectJobHuntingStatus() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testInsertOne() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testUpdateAdvance_or_retreat_to_false() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testUpdateAdvance_or_retreat_to_true() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testUpdateRemark() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testUpdateStatusOne() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testUpdateStatus() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testObject() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testGetClass() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testHashCode() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testEquals() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testClone() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testToString() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testNotify() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testNotifyAll() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testWait() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testWaitLong() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testWaitLongInt() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testFinalize() {
+		fail("まだ実装されていません");
+	}
+
+}
---
+++ src/test/java/jp/ac/hcs/GreenShower/job/request/JobRequestServiceTest.java
@@ -0,0 +1,14 @@
+package jp.ac.hcs.GreenShower.job.request;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JobRequestServiceTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
