6de085f750d7dc070fca12ec75b152e05fb73c0e
s20193050 <s20193050@HCS19015.stdlan.hcs.local>
2021-11-18 09:42:39 +0900

    JobRequestService作業保存

--- src/test/java/jp/ac/hcs/GreenShower/job/request/JobRequestServiceTest.java
+++ src/test/java/jp/ac/hcs/GreenShower/job/request/JobRequestServiceTest.java
@@ -51,7 +51,7 @@ class JobRequestServiceTest {
 		// 2.Do
 		Optional<JobRequestEntity> jobRequestEntity = jobRequestService.selectAllRequests(user_id, role);
 		// 3.Assert
-		assertNotNull(jobRequestEntity);
+		assertTrue(jobRequestEntity.isEmpty());
 		// 4.Logs
 		log.warn("[selectAllRequestメソッドの異常系テスト]reportEntity:" + jobRequestEntity.toString());
 	}
@@ -81,7 +81,7 @@ class JobRequestServiceTest {
 		// 2.Do
 		Optional<JobRequestData> jobRequestData = jobRequestService.selectOne(apply_id, user_id, role);
 		// 3.Assert
-		assertNotNull(jobRequestData);
+		assertTrue(jobRequestData.isEmpty());
 		// 4.Logs
 		log.warn("[selectOneメソッドの異常系テスト]jobRequestData:" + jobRequestData.toString());
 
@@ -107,7 +107,7 @@ class JobRequestServiceTest {
 		// 2.Do
 		Optional<UserData> userData = jobRequestService.selectPersonalInfo(user_id);
 		// 3.Assert
-		assertNotNull(userData);
+		assertTrue(userData.isEmpty());
 		// 4.Logs
 		log.warn("[selectPersonalInfoStringメソッドの異常系テスト]userData:" + userData.toString());
 	}
@@ -194,46 +194,87 @@ class JobRequestServiceTest {
 		log.warn("[hasInsertedの正常系テスト]result:" + result);
 	}
 	
-//	@Test
-//	void hasInsertedの異常系テスト() {
-//		// 1.Ready
-//		List<String> way = new ArrayList<>();
-//		way.add("3");
-//		
-//		JobRequestForm form = new JobRequestForm();
-//		form.setApply_type("1");
-//		form.setCompany_name("株式会社HCS");
-//		form.setDate_activity_from("2021-11-20T09:30");
-//		form.setDate_activity_to("2021-11-21T10:30");
-//		form.setLoc("本校舎9F");
-//		form.setContent("1");
-//		form.setWay(way);
-//		form.setDate_absence_from(null);
-//		form.setDate_absence_to(null);
-//		form.setLeave_early_date(null);
-//		form.setAttendance_date("2021-11-20T09:00");
-//		form.setRemark("筆記試験です。");
-//		
-//		String applicant_id = "isida@xxx.co.jp";
-//		String register_user_id = "isida@xxx.co.jp";
-//		
-//		doThrow(new DataAccessResourceFailureException("")).when(jobRequestRepository).insertOne(anyString());
-//		// 2.Do
-//		boolean result = jobRequestService.hasInserted(form, applicant_id, register_user_id);
-//		// 3.Assert
-//		assertEquals(true, result);
-//		// 4.Logs
-//		log.warn("[hasInsertedの異常系テスト]result:" + result);
-//	}
-
 	@Test
-	void testHasUpdateJobStatus() {
-		fail("まだ実装されていません");
+	void hasInsertedの異常系テスト() {
+		// 1.Ready
+		List<String> way = new ArrayList<>();
+		way.add("3");
+		
+		JobRequestForm form = new JobRequestForm();
+		form.setApply_type("1");
+		form.setCompany_name("株式会社HCS");
+		form.setDate_activity_from("2021-11-20T09:30");
+		form.setDate_activity_to("2021-11-21T10:30");
+		form.setLoc("本校舎9F");
+		form.setContent("1");
+		form.setWay(way);
+		form.setDate_absence_from(null);
+		form.setDate_absence_to(null);
+		form.setLeave_early_date(null);
+		form.setAttendance_date("2021-11-20T09:00");
+		form.setRemark("筆記試験です。");
+		
+		String applicant_id = "isida@xxx.co.jp";
+		String register_user_id = "isida@xxx.co.jp";
+		
+		doThrow(new DataAccessResourceFailureException("")).when(jobRequestRepository).insertOne(any());
+		// 2.Do
+		boolean result = jobRequestService.hasInserted(form, applicant_id, register_user_id);
+		// 3.Assert
+		assertEquals(false, result);
+		// 4.Logs
+		log.warn("[hasInsertedの異常系テスト]result:" + result);
 	}
 
 	@Test
-	void testHasUpdatedJobContent() {
-		fail("まだ実装されていません");
+	void hasUpdateJobStatusの正常系テスト() {
+		// 1.Ready
+		String apply_id = "1";
+		
+		JobRequestForm form = new JobRequestForm();
+		form.setStatus("1");
+		form.setIndicate("申請に不備があります");
+		
+		// 2.Do
+		boolean result = jobRequestService.hasUpdateJobStatus(apply_id, form);
+		// 3.Assert
+		assertEquals(true, result);
+		// 4.Logs
+		log.warn("[hasUpdateJobStatusの正常系テスト]result:" + result);
+	}
+	
+	@Test
+	void hasUpdateJobStatusの異常系テスト() {
+		// 1.Ready
+		String apply_id = "1";
+		
+		JobRequestForm form = new JobRequestForm();
+		form.setStatus("1");
+		form.setIndicate("申請に不備があります");
+		
+		doThrow(new DataAccessResourceFailureException("")).when(jobRequestRepository).updateJobStatus(anyString(),anyString(),anyString());
+		
+		// 2.Do
+		boolean result = jobRequestService.hasUpdateJobStatus(apply_id, form);
+		// 3.Assert
+		assertEquals(false, result);
+		// 4.Logs
+		log.warn("[hasUpdateJobStatusの異常系テスト]result:" + result);
+	}
+
+	@Test
+	void hasUpdatedJobContent() {
+		// 1.Ready
+		String apply_id ="1";
+		String user_id = "1";
+		String role = "学生";
+				doThrow(new DataAccessResourceFailureException("")).when(jobRequestRepository).selectOne(anyString());
+				// 2.Do
+				Optional<JobRequestData> jobRequestData = jobRequestService.selectOne(apply_id, user_id, role);
+				// 3.Assert
+				assertTrue(jobRequestData.isEmpty());
+				// 4.Logs
+				log.warn("[selectOneメソッドの異常系テスト]jobRequestData:" + jobRequestData.toString());
 	}
 
 	@Test
