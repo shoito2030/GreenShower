6a1d8abf4d014bed53e3789e3694b98f9e269ce3
s20193010 <s20193010@HCS19008.stdlan.hcs.local>
2021-11-24 11:23:00 +0900

    Merge branch 'main' of https://github.com/shoito2030/GreenShower.git into main

--- src/main/java/jp/ac/hcs/GreenShower/job/extra/JobExtraController.java
+++ src/main/java/jp/ac/hcs/GreenShower/job/extra/JobExtraController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jp.ac.hcs.GreenShower.job.request.JobRequestController;
+import jp.ac.hcs.GreenShower.job.request.JobRequestForm;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -29,13 +30,31 @@ public class JobExtraController {
 
 		boolean isSuccess = jobExtraService.listRegistion(apply_id,principal.getName());
 
-		// 処理失敗により就職活動申請詳細画面へ
+		// 処理失敗により就職活動申請詳細画面へ 
 		if (isSuccess) {
 			model.addAttribute("msg", "学生の名簿登録が完了しました。");
 			return jobRequestController.getRequestList(principal, model);
 
 		} else {
 			model.addAttribute("errmsg", "学生の名簿登録に失敗しました");
+			return jobRequestController.getRequestList(principal, model);
+		}
+		
+	}
+	
+	@PostMapping("/job/request/status-change/document-receipt")
+	public String documentReceirt(JobRequestForm form,Principal principal,Model model) {
+		String apply_id = (String)session.getAttribute("apply_id");
+
+		boolean isSuccess = jobExtraService.listRegistion(apply_id,principal.getName());
+
+		// 処理失敗により就職活動申請詳細画面へ
+		if (isSuccess) {
+			model.addAttribute("msg", "書類の受取登録が完了しました。");
+			return jobRequestController.getRequestList(principal, model);
+
+		} else {
+			model.addAttribute("errmsg", "書類の受取登録に失敗しました");
 			return jobRequestController.getRequestList(principal, model);
 		}
 		
