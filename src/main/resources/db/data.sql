6bc14d76381d5a884d5fdd1a0ce265c3a56fcb72
s20192030 <s20192030@HCS19007.stdlan.hcs.local>
2021-11-30 10:12:11 +0900

    data

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,3 @@
 INSERT INTO "PUBLIC"."USERS" VALUES
-('t-ukeire@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', STRINGDECODE('\u5b89\u90e8\u83ef\u5948'), 'ROLE_TEACHER', 'S3A1', NULL, FALSE, TIMESTAMP '2021-11-30 09:49:48.236948', 't-ukeire@hcs.ac.jp', NULL, NULL, TRUE, 0, 1),
-('s-ukeire@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', STRINGDECODE('\u77f3\u7530\u60a0\u4ecb'), 'ROLE_STUDENT', 'S3A1', '01', FALSE, TIMESTAMP '2021-11-30 09:49:48.241935', 't-ukeire@hcs.ac.jp', NULL, NULL, TRUE, 0, 1);
+('t-ukeire@hcs.ac.jp', '$2a$10$xRTXvpMWly0oGiu65WZlm.3YL95LGVV2ASFjDhe6WF4.Qji1huIPa', STRINGDECODE('\u5b89\u90e8\u83ef\u5948'), 'ROLE_TEACHER', 'S3A1', NULL, FALSE, TIMESTAMP '2021-11-30 10:07:50.67929', 't-ukeire@hcs.ac.jp', NULL, NULL, TRUE, 0, 1),
+('s-ukeire@hcs.ac.jp', '$2a$10$5iJr0wNAxCEFQF2cjk5ixeZtcz.BQru3LETcoXe/IeVg2gaBlWIKW', STRINGDECODE('\u77f3\u7530\u96c4\u4ecb'), 'ROLE_STUDENT', 'S3A1', '01', FALSE, TIMESTAMP '2021-11-30 10:09:37.416439', 't-ukeire@hcs.ac.jp', NULL, NULL, TRUE, 0, 1);
